1b2493174ea6c003c073d18fb41b45bfc09ccf15
bdykman1994 <bdykman1994@gmail.com>
2022-04-19 10:01:37 -0400

    test

---
+++ Java_Practice_2/src/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 12;
+		
+		int otherNum =7;
+		
+		if(otherNum < num) {
+			System.out.println("yes");
+		}
+	}
+
+}
