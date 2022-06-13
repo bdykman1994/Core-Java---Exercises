bb6ea4fbb54c8d202e6cfe0bdab54b329ce1c991
bdykman1994 <bdykman1994@gmail.com>
2022-06-12 22:48:11 -0400

    syso for authenticated user id. I do not want to lose this code

--- capstone/fitness-app/fitness-app/src/main/java/com/example/fitnessapp/controller/ExerciseController.java
+++ capstone/fitness-app/fitness-app/src/main/java/com/example/fitnessapp/controller/ExerciseController.java
@@ -3,6 +3,8 @@ package com.example.fitnessapp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.fitnessapp.CustomUserDetails;
 import com.example.fitnessapp.entity.Exercise;
 import com.example.fitnessapp.entity.ExerciseType;
+import com.example.fitnessapp.entity.User;
 import com.example.fitnessapp.entity.Workout;
 import com.example.fitnessapp.service.ExerciseService;
 import com.example.fitnessapp.service.WorkoutService;
@@ -21,6 +25,8 @@ import com.example.fitnessapp.service.WorkoutService;
 @RequestMapping(path = "/exercise")
 public class ExerciseController {
 
+
+	
 	@Autowired
 	ExerciseService exerciseService;
 	
@@ -32,6 +38,8 @@ public class ExerciseController {
 	public String viewExerciseHome(Model theModel) {
 		List<Exercise> theExercise = exerciseService.getExercise();
 		theModel.addAttribute("workout", theExercise);
+		System.out.println(((CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsersId());
+		System.out.println( SecurityContextHolder.getContext().getAuthentication().getName());
 		return "exercisePage";
 	}
 	
